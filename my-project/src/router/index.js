721ef576458823ac9a36c4df006cac4afa790d68
gavin <givirnal@givienaldeMacBook-Pro.local>
2017-12-19 19:31:27 +0800

    log-in

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+/*引入页面*/
+import Login from '@/components/login.vue'
+
 
 Vue.use(Router)
 
+/*配置路由*/
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
     }
   ]
 })
