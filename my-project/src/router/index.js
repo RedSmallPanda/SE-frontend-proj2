e1d0befa3c633d3b9485d85b9d2591b493af6d6f
DESKTOP-GO8OUUP\12276 <mytkeroro@sjtu.edu.cn>
2017-12-20 00:00:21 +0800

    0

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Hello from '@/components/Hello'
+import Index from '../views/index/index'
+import Manage from '../views/manage/index'
 
 Vue.use(Router)
 
@@ -8,8 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/manage',
+      name: 'Manage',
+      component: Manage
     }
   ]
 })
