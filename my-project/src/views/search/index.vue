d7a78278ccf36b25813678f75bb818bfa15acea0
LAPTOP-I4HI2762\admin <iwrotetoyou@sjtu.edu.cn>
2017-12-21 23:54:58 +0800

    修复了点赞功能，对页面进行重新布局

--- my-project/src/views/search/index.vue
+++ my-project/src/views/search/index.vue
@@ -7,21 +7,17 @@
 			<button v-on:click="addName">确定</button>
 			<button v-on:click="cancel">取消</button>
 		</div>
-<ul>
-		<li class="content-li" v-for="(value,index) in anwsers">
+
+		<div class="content-li" v-for="(value,index) in anwsers"v-bind:id="index">
 			<div class="header">
 				<img :src="value.photo" />
 				<span class="name">{{value.name}}</span>
-				<span class="com-up" :class="[value.is_uped ? 'is-uped' : ' ']" v-bind:id="index" v-on:click="setUp">{{value.ups}}</span>
-				<span class="problem" v-on:click="report">答案不正确？</span>
-				
+				<span class="com-up" :class="[value.is_uped ? 'is-uped' : ' ']" v-on:click="setUp">{{value.ups}}</span>			
 			</div>
-			<!--<div class="title">
-				<strong>{{value.data.title}}</strong>
-			</div>-->
 			<div class="text-content">{{value.content}}</div>
-		</li>
-	</ul>
+			<span class="problem" v-on:click="report">答案不正确？</span>
+		</div>
+	
 		<!--footer-nav v-bind:class="{'isManage':isNowPage}"></footer-nav-->
 	</div>
 </template>
@@ -33,9 +29,9 @@
 				isNowPage: true,
 				showAdd: false,
 				anwsers:[
-				{'is_uped' : false,photo:'../../static/1.jpg','name':'用户1','content':'此开卷第一回也','ups':6660},
-				{'is_uped' : false,photo:'../../static/2.jpg','name':'用户2','content':'nothing is everything going to waste.','ups':0},
-				{'is_uped' : false,photo:'../../static/3.jpg','name':'用户3','content':'he is no rich.','ups':0}],
+				{'is_uped' : false,photo:'../../static/1.jpg','name':'用户1','content':'此开卷第一回也','ups':135},
+				{'is_uped' : false,photo:'../../static/2.jpg','name':'用户2','content':'nothing is everything going to waste.','ups':47},
+				{'is_uped' : false,photo:'../../static/3.jpg','name':'用户3','content':'he is no rich.','ups':23}],
 				nameValue: '',				
 			}
 		},
@@ -59,56 +55,34 @@
 					newan.content = v
 					newan.ups = 0
 					this.anwsers.push(newan)
+					this.nameValue = ""
 					this.showAdd = false
 				}
 			},
 			setUp(e){
-				var id = e.target.offsetParent.id
-				alert(id)
-				if(this.anwsers[id].is_uped == false){
-				this.anwsers[id].ups++
-				this.anwsers[id].is_uped = true
+				var id2 = e.target.offsetParent.id
+				if(this.anwsers[id2].is_uped == false){
+				this.anwsers[id2].ups++
+				this.anwsers[id2].is_uped = true
 				}
 				else{
-				this.anwsers[id].ups--
-				this.anwsers[id].is_uped = false
+				this.anwsers[id2].ups--
+				this.anwsers[id2].is_uped = false
 				}
 			},
 			report(){
 				alert("已向后台反映，感谢您的反馈！")
 
 			},
-			/*del(e){
-				var id = e.target.offsetParent.id
-				this.peoples.splice(id,1)
-			},
-			edit(e){
-				var id = e.target.offsetParent.id
-				this.showEdit = true
-				this.editId = id
-				this.newName = this.peoples[id].name
-			},
-			cancel(){
-				this.showEdit = false
-			},
-			editName(){
-				var v = this.newName
-				if(v.trim() == ""){
-					alert("请输入图书名称")
-				}else{
-					this.peoples[this.editId].name = this.newName
-					this.showEdit = false
-				}
-			}*/
 		}
 	}
 </script>
 
 <style scoped>
 	.manage{padding-bottom:50px;}
-	.manage >button{width:500px; height:70px; line-height:40px; background-color:#41b883; border: none; border-radius:5px; font-size:30px; color:#fff;}
-	.input-area input{display:input;width: 200px; height: 40px; line-height:40px; margin:20px 0; outline:none; border:1px solid #333;}
-	.input-area button{display:button; width:60px; height: 40px; line-height:40px; background-color:#41b883; border: none; border-radius:5px; font-size:16px; color:#fff;}
+	.manage >button{width:300px; height:50px; line-height:40px; background-color:#41b883; border: none; border-radius:5px; font-size:30px; color:#fff;}
+	.input-area input{width: 800px; height: 40px; line-height:40px; margin:20px 0; outline:none; border:1px solid #333;}
+	.input-area button{ width:60px; height: 40px; line-height:40px; background-color:#41b883; border: none; border-radius:5px; font-size:16px; color:#fff;}
 
 
 	.content-li {
@@ -116,14 +90,16 @@
 		box-sizing: border-box;
 		padding: 0 0.2rem;
 		background: #fff;
-		margin-top: 0.5rem;
-		box-shadow: 0 -2px 2px rgba(213, 215, 217, .8),
+		margin-top: 0.7rem;
+		margin-left: 10rem;
+		margin-right: 10rem;
+		box-shadow: 0 -4px 4px rgba(213, 215, 217, .8),
 		0 2px 2px rgba(213, 215, 217, .8);		
 		}
            .header {
-			 position:relative;
+			/* position:relative;*/
 			    height: 1rem;
-			    padding:0.2rem 0;
+			    padding:1rem 1.5rem;
 			}
 			img {
 				height:5rem;
@@ -145,14 +121,15 @@
 			.problem{
 					float: right;
 					width:7rem;
-					height: 1.5rem;
+					height: 1.2rem;
 					text-align: right;
 					line-height: 0.5rem;
 					color: #FFFFFF;
 					background-color:#999;
 					border-radius:5px; 
-					padding-top:0.7rem;
-					position:absolute; right:0rem; top:2rem;
+					 margin-right:1rem;
+					 margin-top:-2.2rem;
+					 padding-top :0.5rem;
 			
 				}
 				.com-up {
