f51e9645945b3bbae05304dbbd7885c9abc41d35
LAPTOP-I4HI2762\admin <iwrotetoyou@sjtu.edu.cn>
2017-12-21 19:38:00 +0800

    解决用户头像无法正常显示的问题，新增点赞按钮

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+//import Hello from '@/components/Hello'
+//import Index from '../views/index/index'
+import Manage from '../views/search/index'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+  /*  {
+      	path: '/',
+      	name: 'Index',
+      	component: Index
+    },*/
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+    	path: '/',
+       	name: 'Manage',
+   		component: Manage
     }
   ]
 })
---
+++ my-project/src/views/search/index.vue
@@ -0,0 +1,186 @@
+<template>
+	<div class="manage tc">
+	
+		<button v-on:click="add">添加答案</button>
+		<div class="input-area" v-show="showAdd">
+			<input type="text" placeholder="请输入您的答案" v-model="nameValue">
+			<button v-on:click="addName">确定</button>
+			<button v-on:click="cancel">取消</button>
+		</div>
+	<ul class="content-ul">
+		<li class="content-li" v-for="(value,index) in anwsers">
+			<div class="header">
+				<img :src="value.photo" />
+				<span class="name">{{value.name}}</span>
+				<span class="com-up" :class="[value.is_uped ? 'is-uped' : ' ']" v-bind:id="index" v-on:click="setUp">{{value.ups}}</span>
+				<span class="problem" v-on:click="report">答案不正确？</span>
+				
+			</div>
+			<!--<div class="title">
+				<strong>{{value.data.title}}</strong>
+			</div>-->
+			<div class="text-content">{{value.content}}</div>
+		</li>
+		</ul>
+		<!--footer-nav v-bind:class="{'isManage':isNowPage}"></footer-nav-->
+	</div>
+</template>
+
+<script>
+	//import footerNav from '../../components/footer.vue'
+	export default{
+		/*components: {
+			footerNav
+		},*/
+		data(){
+			return{
+				isNowPage: true,
+				showAdd: false,
+				//lightened:false,
+				//upNum:0,
+				anwsers:[
+				{'is_uped' : false,photo:'../../static/1.jpg','name':'用户1','content':'此开卷第一回也。作者自云：曾历过一番梦幻之后，故将真事隐去，而借通灵说此《石头记》一书也，故曰“甄士隐”云云。但书中所记何事何人?自己又云：“今风尘碌碌，一事无成，忽念及当日所有之女子：一一细考较去，觉其行止见识皆出我之上。我堂堂须眉诚不若彼裙钗，我实愧则有馀，悔又无益，大无可如何之日也。当此日，欲将已往所赖天恩祖德，锦衣纨之时，饫甘餍肥之日，背父兄教育之恩，负师友规训之德，以致今日一技无成、半生潦倒之罪，编述一集，以告天下；知我之负罪固多，然闺阁中历历有人，万不可因我之不肖，自护己短，一并使其泯灭也。','ups':0},
+				{'is_uped' : false,photo:'../../static/2.jpg','name':'用户2','content':'nothing is everything going to waste.','ups':0},
+				{'is_uped' : false,photo:'../../static/3.jpg','name':'用户3','content':'he is no rich.','ups':0}],
+				nameValue: '',				
+			}
+		},
+		methods: {
+			add(){
+				this.showAdd = true
+			},
+			cancel(){
+			this.showAdd = false
+				alert("ooooo")
+			},
+			addName(){
+				var v = this.nameValue
+				if(v.trim() == ""){
+					alert("答案不能为空")
+				}else{
+					var newan = {}
+					newan.is_uped = false
+					newan.photo = '../../static/4.jpg'		
+					newan.name = '新用户'
+					newan.content = v
+					newan.ups = 0
+					this.anwsers.push(newan)
+					//this.showAdd = false
+				}
+			},
+			sueUp(e){
+				var id = e.target.offsetParent.id
+				if(this.anwsers[id].is_uped == false){
+				this.anwsers[id].ups++
+				this.anwsers[id].is_uped = true
+				}
+				else{
+				this.anwsers[id].ups--
+				this.anwsers[id].is_uped = false
+				}
+			},
+			report(){
+				alert("已向后台反映，感谢您的反馈！")
+
+			},
+			/*del(e){
+				var id = e.target.offsetParent.id
+				this.peoples.splice(id,1)
+			},
+			edit(e){
+				var id = e.target.offsetParent.id
+				this.showEdit = true
+				this.editId = id
+				this.newName = this.peoples[id].name
+			},
+			cancel(){
+				this.showEdit = false
+			},
+			editName(){
+				var v = this.newName
+				if(v.trim() == ""){
+					alert("请输入图书名称")
+				}else{
+					this.peoples[this.editId].name = this.newName
+					this.showEdit = false
+				}
+			}*/
+		}
+	}
+</script>
+
+<style scoped>
+	.manage{padding-bottom:50px;}
+	.manage >button{width:500px; height:70px; line-height:40px; background-color:#41b883; border: none; border-radius:5px; font-size:30px; color:#fff;}
+	.input-area input{display:input;width: 200px; height: 40px; line-height:40px; margin:20px 0; outline:none; border:1px solid #333;}
+	.input-area button{display:button; width:60px; height: 40px; line-height:40px; background-color:#41b883; border: none; border-radius:5px; font-size:16px; color:#fff;}
+
+
+	.content-li {
+		box-sizing: border-box;
+		padding: 0 0.2rem;
+		background: #fff;
+		margin-top: 0.5rem;
+		box-shadow: 0 -2px 2px rgba(213, 215, 217, .8),
+		0 2px 2px rgba(213, 215, 217, .8);		
+		}
+           .header {
+			 position:relative;
+			    height: 1rem;
+			    padding:0.2rem 0;
+			}
+			img {
+				height:5rem;
+				float: left;
+				border-radius:50%;
+				 padding:0.5rem 0;
+			}
+			span {
+				float: left;
+				height: 100%;
+				margin-left: 0.2rem;
+				line-height: 0.7rem;
+			}
+			.name {
+				color: #99A4AA;
+				 padding:2.5rem 0;
+				 font-size:1rem
+			}
+			.problem{
+					float: right;
+					width:7rem;
+					height: 1.5rem;
+					text-align: right;
+					line-height: 0.5rem;
+					color: #FFFFFF;
+					background-color:#999;
+					border-radius:5px; 
+					padding-top:0.7rem;
+					position:absolute; right:0rem; top:2rem;
+			
+				}
+				.com-up {
+					float: right;
+					width: 3rem;
+					height: 1.5rem;
+					text-align: right;
+					background: url("../../assets/upup.svg") no-repeat 30%;
+					background-size: contain;
+					line-height: 0.5rem;
+					color: #999;
+					position:absolute;
+					right:2rem;
+				}
+				.is-uped {
+					background: url("../../assets/upup2.svg") no-repeat 30%;
+					background-size: contain;
+				}
+			
+		.text-content {
+			color: #474747;
+			font-size:1.0rem;
+			line-height:2rem;
+			padding: 3rem 3rem;
+		}
+		
+</style>
