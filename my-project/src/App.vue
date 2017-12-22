91b3b4e288af4885c6c6a96bcea6f40e18c24fc8
LAPTOP-I4HI2762\admin <iwrotetoyou@sjtu.edu.cn>
2017-12-22 21:46:28 +0800

    设置页面背景为灰色

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -15,12 +15,13 @@
 
   @import './assets/css/public.css';
   #app {
-    /*background-color: rgba(213, 215, 217, .15);*/
+    background-color: rgba(213, 215, 217, .15);
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    margin-top: 0px;
+    padding-bottom: 25rem
   }
 </style>
